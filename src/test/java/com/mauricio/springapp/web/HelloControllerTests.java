a65cde8700151a07f7682deded5e830996d4b27a
Luis Mauricio Alamo <luismalamoc@gmail.com>
2017-07-08 12:43:07 -0400

    Capítulo 2. Desarrollando y Configurando la Vista y el Controlador Parte 1

--- src/test/java/com/mauricio/springapp/web/HelloControllerTests.java
+++ src/test/java/com/mauricio/springapp/web/HelloControllerTests.java
@@ -1,15 +1,22 @@
 package com.mauricio.springapp.web;
 
 import static org.junit.Assert.*;
+import com.mauricio.springapp.web.HelloController;
 
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 public class HelloControllerTests {
 	@Test
     public void testHandleRequestView() throws Exception{		
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);		
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        assertEquals("WEB-INF/views/hello.jsp", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+        String nowValue = (String) modelAndView.getModel().get("now");
+        assertNotNull(nowValue);
+
     }
 }
